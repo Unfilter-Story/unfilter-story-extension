fe1eb45259f75a9d30f142042f1e920fdbf88b62
vrundalad <vrundalad4099@gmail.com>
2026-06-15 14:27:13 +0530

    mediaheaderdeopdown

--- js/components/AppHeader.js
+++ js/components/AppHeader.js
@@ -26,12 +26,20 @@ class AppHeader extends HTMLElement {
 
           <nav class="nav-desktop">
             <ul>
-              <li><a href="news.html" ${isPage('news.html')}>News</a></li>
-              <li><a href="articles.html" ${isPage('articles.html')}>Articles</a></li>
+              <li class="dropdown">
+                <a href="#" style="cursor: default;">
+                  Media
+                  <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" style="margin-top: 1px;"><polyline points="6 9 12 15 18 9"></polyline></svg>
+                </a>
+                <ul class="dropdown-menu">
+                  <li><a href="news.html" ${isPage('news.html')}>News</a></li>
+                  <li><a href="articles.html" ${isPage('articles.html')}>Articles</a></li>
+                  <li><a href="interviews.html" ${isPage('interviews.html')}>Interviews</a></li>
+                </ul>
+              </li>
               <li><a href="stories.html" ${isPage('stories.html')}>Startup Stories</a></li>
               <li><a href="ai-innovation.html" ${isPage('ai-innovation.html')}>AI & Innovation</a></li>
               <li><a href="funding.html" ${isPage('funding.html')}>Funding</a></li>
-              <li><a href="interviews.html" ${isPage('interviews.html')}>Interviews</a></li>
               <li><a href="about.html" ${isPage('about.html')}>About</a></li>
               <li><a href="contact.html" ${isPage('contact.html')}>Contact</a></li>
             </ul>
@@ -62,12 +70,17 @@ class AppHeader extends HTMLElement {
         <nav id="mobile-drawer" class="mobile-drawer" aria-hidden="true">
           <ul>
             <li><a href="index.html">Home</a></li>
-            <li><a href="news.html">News</a></li>
-            <li><a href="articles.html">Articles</a></li>
+            <li class="dropdown-group">
+              <span class="dropdown-title">Media</span>
+              <ul class="dropdown-items">
+                <li><a href="news.html">News</a></li>
+                <li><a href="articles.html">Articles</a></li>
+                <li><a href="interviews.html">Interviews</a></li>
+              </ul>
+            </li>
             <li><a href="stories.html">Startup Stories</a></li>
             <li><a href="ai-innovation.html">AI & Innovation</a></li>
             <li><a href="funding.html">Funding</a></li>
-            <li><a href="interviews.html">Interviews</a></li>
             <li><a href="about.html">About</a></li>
             <li><a href="contact.html">Contact</a></li>
           </ul>
